a7ea5e424bb60161aefe1fbf8736039336b19870
Kimberly Encinas <kimbencinas@gmail.com>
2024-07-27 16:24:58 -0700

    add tabs for navigaation

---
+++ frontend/app/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export default function App() {
+
+    return (
+        <View>
+            <Text>hi</Text>
+        </View>
+    )
+}
