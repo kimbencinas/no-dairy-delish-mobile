86a7b91fc9449d174ddd80412999ad79e5891e39
Kimberly Encinas <kimbencinas@gmail.com>
2024-08-01 00:48:35 -0700

    Set up react native router

---
+++ frontend/app/_layout.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import home from './(tabs)/home'
+
+const AppLayout = () => {
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={home}
+                />
+                <Stack.Screen
+
+                />
+                <Stack.Screen
+
+                />
+                <Stack.Screen
+
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default AppLayout;
--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, ScrollView } from 'react-native'
 
 export default function App() {
 
     return (
-        <View>
-            <Text>hi</Text>
-        </View>
+        <ScrollView>
+            <View>
+                <Text>NoDairyDelish</Text>
+            </View>
+        </ScrollView>
     )
 }
