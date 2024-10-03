9e70b38c8732c6abef824f819d985b851bb4464b
Kimberly Encinas <kimbencinas@gmail.com>
2024-10-02 23:53:17 -0700

    changed file structure

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import { Text, View, StyleSheet, ScrollView } from 'react-native'
+import { Text, View, StyleSheet, ScrollView, StatusBar } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
 
     return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView>
-                <View>
-                    <Text>NoDairyDelish</Text>
-                </View>
-            </ScrollView>
+        <SafeAreaView>
+            <Text>NoDairyDelish</Text>
         </SafeAreaView>
     )
 }
-
-const styles = StyleSheet.create({
-
-});
--- frontend/app/screens/home.jsx
+++ frontend/app/screens/home.jsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Redirect } from 'expo-router'
 
 const Home = () => {
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <div>
+      
+    </div>
   )
 }
 
